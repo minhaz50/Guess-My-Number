5cdce7bb61fab6dba0ec627f6700f5404c6f18e4
Minhaz <md.minhazuddin2597@gmail.com>
2024-02-15 21:00:13 +0600

    Added event listner for Check button for  checking the number

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+/*
+document.querySelector('.message').textContent = '🎉 Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 14;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+});
